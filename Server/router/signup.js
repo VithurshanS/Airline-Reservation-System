3e14e93b0726ac863e31945b57620d010ec36ff3
vithurshan <vithurshans.22@uom.lk>
2024-10-08 21:47:04 +0530

    login and signup upgraded

--- Server/router/signup.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addPassengerDetails} = require('../controller/passengerController');
-
-router.post('/details', addPassengerDetails);
-
-module.exports = router;
--- Server/router/user.js
+++ Server/router/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {addDetails,getDetails} = require("../controller/user");
 
+
 router.post("/add",addDetails);
 router.get("/get",getDetails);
 
