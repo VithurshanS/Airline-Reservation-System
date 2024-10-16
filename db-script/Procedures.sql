03cea9925765bfa29309a2061027462223948ee3
vithurshan <vithurshans.22@uom.lk>
2024-10-16 06:26:08 +0530

    insertstatements

--- db-script/Procedures.sql
+++ db-script/Procedures.sql
@@ -1,55 +1,4 @@
-DELIMITER $$
-
-CREATE PROCEDURE handleLogin(
-    IN username VARCHAR(200),
-    IN pass VARCHAR(10),
-    OUT mes INT
-)
-BEGIN
-    DECLARE user_count INT DEFAULT 0;
-    DECLARE is_passCorrect INT DEFAULT 0;
-
-    select count(*) into user_count from User where User_Name = username;
-    if user_count = 0 then 
-        set mes = 404;
-    else
-        select count(*) into is_passCorrect from user where (User_Name,Password) = (username,pass);
-        if is_passCorrect = 0 then
-            set mes = 304;
-        else
-            set mes = 201;
-            select * from user where (User_Name,Password) = (username,pass);
-
-        end if;
-    end if;
-end $$
-DELIMITER ;
-
-
-DELIMITER $$
-CREATE PROCEDURE handleSignin(
-    IN username
-
-)
-
-DELIMITER;  
-
-
-DELIMITER $$
-CREATE PROCEDURE handleRouteadd(
-    IN Depature_Airport VARCHAR(5),
-    IN Arival_Airport VARCHAR(5),
-)
-BEGIN
-    DECLARE route_count INT DEFAULT 0;
-    SELECT COUNT(*) INTO route_count FROM route WHERE Depature_Airport = Depature_Airport AND Arival_Airport = Arival_Airport;
-    IF route_count = 0 THEN
-        INSERT INTO route (Deature_Airport, Arival_Airport) VALUES (Deature_Airport, Arival_Airport);
-    END IF;
-END $$
-
-DELIMITER ;
-
+DROP PROCEDURE if exists handleAircraft;
 DELIMITER $$
 CREATE PROCEDURE handleAircraft(
    IN Comp VARCHAR(255),
@@ -76,6 +25,9 @@ END $$
 DELIMITER ;
 
 
+
+drop procedure if exists addAddress;
+DROP PROCEDURE addAddress;
 DELIMITER $$
 CREATE PROCEDURE addAddress(
     IN PLID INT,
@@ -99,6 +51,9 @@ BEGIN
 END $$;
 DELIMITER ;
 
+
+
+drop procedure if exists getLocation;
 DELIMITER $$
 CREATE PROCEDURE getLocation(
     IN Loc_ID INT
@@ -118,7 +73,3 @@ END $$;
 
 
 DELIMITER ;
-
-//`INSERT INTO User (User_ID,;User_Name,First_name,Last_name,Email,DOB,Age,Gender,Password,Role)
-
-
