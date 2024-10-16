07c095852583827b6bac8f1b2f4939df6c72baad
vithurshan <vithurshans.22@uom.lk>
2024-10-16 21:28:15 +0530

    ff

--- Server/controller/seatController.js
+++ Server/controller/seatController.js
@@ -3,9 +3,8 @@ const db = require('../database');
 
 exports.addSeat = async (req,res)=>{
     const {Schedule_ID,Seat_number,Seat_class} = req.body;
-    const primarykey = `${Schedule_ID}${Seat_number}`;
-    const insertSeat = `INSERT INTO seat (Seat_ID, Schedule_ID, Seat_Number, Seat_Class) VALUES (?,?,?,?);`;
-    db.query(insertSeat,[primarykey,Schedule_ID,Seat_number,Seat_class],(error,result)=>{
+    const insertSeat = `INSERT INTO seat (Seat_ID, Schedule_ID, Seat_Number, Seat_Class) VALUES (UUID(),?,?,?);`;
+    db.query(insertSeat,[Schedule_ID,Seat_number,Seat_class],(error,result)=>{
         if(error){
             console.log(error);
             res.status(500).send({"message":"Failed to add seat."});
