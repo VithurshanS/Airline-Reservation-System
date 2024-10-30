4adbc68006b3b8a5f3c423ef8633420f3cfce183
vithurshan <vithurshans.22@uom.lk>
2024-10-30 15:15:04 +0530

    jj

--- New folder/Server/router/plane.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {addPlane,getPlane} = require('../controller/planeController');
-
-router.post('/addplane',addPlane);
-router.get('/getplane/:id',getPlane);
-
-
-module.exports = router;
--- New folder/Server/router/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {addRoute,getRoute} = require('../controller/routeController');
-
-router.post('/addroute',addRoute);
-router.get('/getroute/:id',getRoute);
-
-module.exports = router;
