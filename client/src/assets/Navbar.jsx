74af5cabcd70a3908f07d1463deb9044433dee1d
Dinushka-Aththanayake <161476349+Dinushka-Aththanayake@users.noreply.github.com>
2024-10-30 07:44:12 +0530

    modified

--- client/src/assets/Navbar.jsx
+++ client/src/assets/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-r from-purple-600 via-gray-800 to-black text-white p-4 shadow-lg">
+    <nav class="bg-blue-900 text-white p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
         <div className="logo">
           <h2 className="text-2xl font-bold tracking-wide">Your Logo</h2>
@@ -16,6 +16,10 @@ const Navbar = () => {
           <li>
             <Link to="/about" className="hover:text-gray-100 hover:underline transition-colors duration-300">About</Link>
           </li> */}
+           <li>
+            <Link to="/Home" className="hover:underline transition-all duration-300 border border-white px-4 py-2 rounded-full
+            hover:bg-white  hover:text-black">Home</Link>
+          </li>
           <li>
             <Link to="/login" className="hover:underline transition-all duration-300 border border-white px-4 py-2 rounded-full hover:bg-white hover:text-black">Login</Link>
           </li>
@@ -23,6 +27,7 @@ const Navbar = () => {
             <Link to="/signup" className="hover:underline transition-all duration-300 border border-white px-4 py-2 rounded-full
             hover:bg-white  hover:text-black">Signup</Link>
           </li>
+         
         </ul>
       </div>
     </nav>
